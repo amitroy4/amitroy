7942313c7bada578baae2038b1c9bbfe87505775
amitroy4 <amitroyrock6071@gmail.com>
2023-07-11 00:51:37 +0600

    about me

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,14 @@ function App() {
       <section className='about'>
         <div className="container">
           <div className="about_img">scv</div>
-          <div className="about_text">svs</div>
+          <div className="about_text">
+            <h5>About Me</h5>
+            <div className="para">
+              <p>I am a highly motivated fresher with a strong passion for MERN full-stack web development, possessing  a comprehensive understanding of the MERN full stack, including MongoDB, Express.js, React.js, and Node.js. My skilled areas are HTML5, CSS3, and JavaScript with experience in RESTful API development and integration. I am ready to share knowledge and contribute to the growth and success of aspiring developers as a software engineer.</p>
+              <p>I’m now preparing myself for MERN full-stack web development. My future goal is to make web applications more powerful with strong security.</p>
+            </div>
+            <h5>Hire Me</h5>
+          </div>
         </div>
       </section>
       {/* ------------------------ About Me End ------------------------ */}
